c0b5069028c3327094385ac7c99a195dd7cf4fc2
niktech software <info@niktechsoftware.com>
2019-08-26 02:40:23 +0530

    changes of server Backup

--- application/controllers/singleTeacherControllers.php
+++ application/controllers/singleTeacherControllers.php
@@ -47,11 +47,11 @@
 				//echo $cl_date;
 			}
 		}
-		$data['pageTitle'] = 'Teacher & Employee Dashboard';
+		$data['pageTitle'] = 'Dashboard';
 		$data['smallTitle'] = 'Overview of all Section';
-		$data['mainPage'] = 'Teacher & Employee Dashboard';
-		$data['subPage'] = 'Teacher & Employee Dashboard';
-		$data['title'] = 'Niktech School ERP Dashboard';
+		$data['mainPage'] = 'Dashboard';
+		$data['subPage'] = 'dashboard';
+		$data['title'] = 'Hgenesis Dashboard';
 		$data['headerCss'] = 'headerCss/dashboardCss';
 		$data['footerJs'] = 'footerJs/dashboardJs';
 		$data['mainContent'] = 'teacherdashboard';
@@ -131,23 +131,19 @@
 		}
 	}
 	
-   function deleteleave(){
-   $id=	$this->input->post("leaveid");
-   	$this->db->where('id',$id);
-   	$this->db->where('school_code',$this->session->userdata('school_code'));
-   	$up=$this->db->delete('emp_leave');
-  
-   }
+
 	
 	function viewProfile(){
-		$this->load->model("employeemodel");
+		$this->load->model("allFormModel");
 		$tID = $this->session->userdata('username');
-		$tid=$this->employeemodel->getEmployeProfile($tID);
-		if($tid->num_rows()>0){
+		$this->db->where("username",$tID);
+		$tid=$this->db->get("employee_info");
 		$id= $tid->row();
-		//$teacherID=$id->id;
-		//$stDetail = $this->singleTeacherModel->getTeacherDetail($teacherID);
-		$data['teacherProfile'] = $id;
+		
+		$teacherID=$id->id;
+		
+		$stDetail = $this->singleTeacherModel->getTeacherDetail($teacherID);
+		$data['teacherProfile'] = $stDetail->row();
 		$data['pageTitle'] = 'Teacher Profile';
 		$data['smallTitle'] = 'Teacher Personal Detail';
 		$data['mainPage'] = 'Teacher';
@@ -157,24 +153,14 @@
 		$data['footerJs'] = 'footerJs/singleTeacherJs';
 		$data['mainContent'] = 'teacherProfile';
 		$this->load->view("includes/mainContent", $data);
-	}else{
-		$data['pageTitle'] = 'Teacher Profile';
-		$data['smallTitle'] = 'Teacher Personal Detail';
-		$data['mainPage'] = 'Teacher';
-		$data['subPage'] = 'Teacher Personal Detail';
-		$data['title'] = 'Teacher Personal Detail';
-		$data['headerCss'] = 'headerCss/singleTeacherCss';
-		$data['footerJs'] = 'footerJs/singleTeacherJs';
-		$data['mainContent'] = 'error';
-		$this->load->view("includes/mainContent", $data);
 	}
 	
-	}
+	
 	
        function salarySummry(){
         $emp_id = $this->session->userdata("username");
         $school_code = $this->session->userdata("school_code");
-        $data['var'] = $this->db->query("select * from emp_salary_info where school_code='$school_code' AND emp_id ='$emp_id'");
+        $data['var'] = $this->db->query("select * from emp_salary_info where school_code='$school_code' AND emp_id ='$emp_id'")->result();
         $data['pageTitle'] = 'Teacher Section';
         $data['smallTitle'] = 'Teacher Summry';
         $data['mainPage'] = 'Teacher';
@@ -187,14 +173,6 @@
     }
 	
 	function teacherLeave(){
-		$this->load->model("employeemodel");
-		$tID = $this->session->userdata('username');
-		$tid=$this->employeemodel->getEmployeProfile($tID);
-		if($tid->num_rows()>0){
-		$id= $tid->row()->id;
-		
-		$var = $this->singleTeacherModel->getTeacherLeave($id);
-		$data['teacherLeave'] = $var;
 		$data['pageTitle'] = 'Teacher Section';
 		$data['smallTitle'] = 'Teacher Leave Details';
 		$data['mainPage'] = 'Teacher';
@@ -204,23 +182,13 @@
 		$data['footerJs'] = 'footerJs/singleTeacherJs';
 		$data['mainContent'] = 'teacherLeave';
 		$this->load->view("includes/mainContent", $data);
-	}else{
-		$data['pageTitle'] = 'Teacher Profile';
-		$data['smallTitle'] = 'Teacher Personal Detail';
-		$data['mainPage'] = 'Teacher';
-		$data['subPage'] = 'Teacher Personal Detail';
-		$data['title'] = 'Teacher Personal Detail';
-		$data['headerCss'] = 'headerCss/singleTeacherCss';
-		$data['footerJs'] = 'footerJs/singleTeacherJs';
-		$data['mainContent'] = 'error';
-		$this->load->view("includes/mainContent", $data);
 	}
-	}
+
 	
 	function classTaken(){
 		$stu_id = $this->session->userdata('username');
 		$var1 = $this->singleTeacherModel->time_Table($stu_id);
-		$data['timetable']=$var1;
+		$data['timetable']=$var1->result();
 		$data['pageTitle'] = 'Teacher Section';
 		$data['smallTitle'] = 'Teacher Class Detail';
 		$data['mainPage'] = 'Teacher';
@@ -451,9 +419,11 @@
 		$school_code=$this->session->userdata('school_code');
 		$fsd=$this->session->userdata('fsd');
 		//print_r($fsd);
-		$this->load->model("employeemodel");
-		$empid = $this->employeemodel->getEmployeProfile($v)->row()->id;
-		
+		$this->db->where('username',$v);
+		$this->db->where('school_code',$school_code);
+		$this->db->where('fsd',$fsd);
+		$this->db->where('status',1);
+		$empid=$this->db->get('employee_info')->row()->id;
 		
 		$request=$this->db->query("SELECT * FROM teacher_attendance WHERE emp_id = '$empid' AND a_date >'$start_date' AND a_date < '$end_date' AND school_code='$school_code'");
 		
@@ -462,13 +432,12 @@
 		    	<hr>
 		    	<div class="panel-body">
       <div class="alert alert-info">
-        <button data-dismiss="alert" class="close"></button>
+        <button data-dismiss="alert" class="close">×</button>
         <h3 class="media-heading text-center"> Welcome to Attendence Report Area</h3>
        <p class="media-timestamp"> 
-       Here you can Your Attendance Record to select starting date to end date which are you selected.
+       Here you can see all attendence of you class from starting date to end date which are you selected.
        </p>
       </div>
-      <?php if($request->num_rows()>0){?>
                     <div class="row">
 						<div class="col-md-12 space20">
 							<div class="btn-group pull-right">
@@ -477,7 +446,16 @@
 								</button>
 								<?php if($this->session->userdata('login_type') == 'admin'){?>
 								<ul class="dropdown-menu dropdown-light pull-right">
-							
+									<!--<li>-->
+									<!--	<a href="#" class="export-pdf" data-table="#sample-table-2" >-->
+									<!--		Save as PDF-->
+									<!--	</a>-->
+									<!--</li>-->
+									<!--<li>-->
+									<!--	<a href="#" class="export-png" data-table="#sample-table-2">-->
+									<!--		Save as PNG-->
+									<!--	</a>-->
+									<!--</li>-->
 									<li>
 										<a href="#" class="export-csv" data-table="#sample-table-2" >
 											Save as CSV
@@ -488,7 +466,21 @@
 											Save as TXT
 										</a>
 									</li>
-									
+									<!--<li>-->
+									<!--	<a href="#" class="export-xml" data-table="#sample-table-2" data-ignoreColumn ="3,4">-->
+									<!--		Save as XML-->
+									<!--	</a>-->
+									<!--</li>-->
+									<!--<li>-->
+									<!--	<a href="#" class="export-sql" data-table="#sample-table-2" data-ignoreColumn ="3,4">-->
+									<!--		Save as SQL-->
+									<!--	</a>-->
+									<!--</li>-->
+									<!--<li>-->
+									<!--	<a href="#" class="export-json" data-table="#sample-table-2" data-ignoreColumn ="3,4">-->
+									<!--		Save as JSON-->
+									<!--	</a>-->
+									<!--</li>-->
 									<li>
 										<a href="#" class="export-excel" data-table="#sample-table-2" >
 											Export to Excel
@@ -550,10 +542,6 @@
 							</tbody>
 						</table>
 						</div>
-						<?php }else{?>
-						<div class = "alert alert-warning"><strong><h1>Attendance Record Not Found!!!!!</h1></strong></div>
-						<?php }?>
-						
 						</div>
 						<?php
 	}
--- application/views/teacherLeave.php
+++ application/views/teacherLeave.php
@@ -3,207 +3,196 @@
 		<!-- start: RESPONSIVE TABLE PANEL -->
 		<div class="panel panel-white">
 			<div class="panel-heading panel-orange">
-				<h4 class="panel-title">
-					Teacher <span class="text-bold">Leave Detail</span>
-				</h4>
+				<h4 class="panel-title">Teacher <span class="text-bold">Leave Detail</span></h4>
 				<div class="panel-tools">
 					<div class="dropdown">
-						<a data-toggle="dropdown"
-							class="btn btn-xs dropdown-toggle btn-transparent-grey"> <i
-							class="fa fa-cog"></i>
+						<a data-toggle="dropdown" class="btn btn-xs dropdown-toggle btn-transparent-grey">
+							<i class="fa fa-cog"></i>
 						</a>
 						<ul class="dropdown-menu dropdown-light pull-right" role="menu">
-							<li><a class="panel-collapse collapses" href="#"><i
-									class="fa fa-angle-up"></i> <span>Collapse</span> </a></li>
-							<li><a class="panel-refresh" href="#"> <i class="fa fa-refresh"></i>
-									<span>Refresh</span>
-							</a></li>
-							<li><a class="panel-config" href="#panel-config"
-								data-toggle="modal"> <i class="fa fa-wrench"></i> <span>Configurations</span>
-							</a></li>
-							<li><a class="panel-expand" href="#"> <i class="fa fa-expand"></i>
-									<span>Fullscreen</span>
-							</a></li>
+							<li>
+								<a class="panel-collapse collapses" href="#"><i class="fa fa-angle-up"></i> <span>Collapse</span> </a>
+							</li>
+							<li>
+								<a class="panel-refresh" href="#">
+									<i class="fa fa-refresh"></i> <span>Refresh</span>
+								</a>
+							</li>
+							<li>
+								<a class="panel-config" href="#panel-config" data-toggle="modal">
+									<i class="fa fa-wrench"></i> <span>Configurations</span>
+								</a>
+							</li>
+							<li>
+								<a class="panel-expand" href="#">
+									<i class="fa fa-expand"></i> <span>Fullscreen</span>
+								</a>
+							</li>
 						</ul>
 					</div>
-					<a class="btn btn-xs btn-link panel-close" href="#"> <i
-						class="fa fa-times"></i>
+					<a class="btn btn-xs btn-link panel-close" href="#">
+						<i class="fa fa-times"></i>
 					</a>
 				</div>
 			</div>
 			<div class="panel-body">
-				<div class="alert alert-info">
-					<h3 class="media-heading text-center">Welcome to Teacher Leave
-						Detail Area,</h3>
-					<p class="media-timestamp">Here you can see your leave details.If
-						your leave is Approved then you will see Approved Button. You can
-						also request for Leave by click on Define New Leave Button.</p>
-				</div>
-				<?php if($this->uri->segment("3")=="Success"){?>
-				<div class = "alert alert-danger">
-				Leave Details are Successfully Deleted!!!!!!!
-				</div>
-				<?php }?>
+			    <div class="alert alert-info"><h3 class="media-heading text-center">Welcome to Teacher Leave Detail Area,</h3>
+                    <p class="media-timestamp">
+                        Here you can see your leave details.If your leave is Approved then you will see Approved Button.
+                        You can also request for Leave by click on Define New Leave Button. 
+                    </p>
+                </div>
 				<div class="form-group">
-					<div class="col-sm-12">
-						<div class="row">
-							<div class="col-md-12 space20">
-								<div class="btn-group pull-right">
-									<button data-toggle="dropdown"
-										class="btn btn-green dropdown-toggle">
-										Export <i class="fa fa-angle-down"></i>
-									</button>
-									<ul class="dropdown-menu dropdown-light pull-right">
-										<li><a href="#" class="export-excel"
-											data-table="#sample-table-2" data-ignoreColumn="3,4"> Export
-												to Excel </a></li>
-									</ul>
-								</div>
-							</div>
-
-							<div class="table-responsive">
-							<?php if($teacherLeave->num_rows()>0){?>
-								<table class="table table-striped table-hover" id="example">
-									<thead>
-										<tr>
-											<th>S.no.</th>
-											<th>Start Date</th>
-											<th>End Date</th>
-											<th>Total leave</th>
-											<td>Reason</td>
-											<th>Action</th>
-
-										</tr>
-									</thead>
-									<tbody>
-			<?php
-			$color = array (
-					"progress-bar-danger",
-					"progress-bar-success",
-					"progress-bar-warning",
-					"progress-partition-green",
-					"partition-azure",
-					"partition-blue",
-					"partition-orange",
-					"partition-purple",
-					"progress-bar-danger",
-					"progress-bar-success",
-					"progress-partition-green",
-					"partition-purple" 
-			);
+				<div class="col-sm-12">	
+			<div class="row">
+				<div class="col-md-12 space20">
+				<div class="btn-group pull-right">
+				<button data-toggle="dropdown" class="btn btn-green dropdown-toggle">
+					Export <i class="fa fa-angle-down"></i>
+				</button>
+				<ul class="dropdown-menu dropdown-light pull-right">
+					<li>
+						<a href="#" class="export-excel" data-table="#sample-table-2" data-ignoreColumn ="3,4">
+							Export to Excel
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 			
-			$count = 1;
-			foreach ( $teacherLeave->result () as $lv ) :
-				?>
+	<div class="table-responsive">
+		<table class="table table-striped table-hover" id="example">
+			<thead>
+				<tr>
+					<th>S.no.</th>
+					<th>Start Date</th>
+					<th>End Date</th>
+					<th>Total leave</th>
+					<td>Reason</td>
+					<th>Action</th>
+					
+				</tr>
+			</thead>
+			<tbody>
+			<?php 
+		
+			    $color = array(
+				    "progress-bar-danger",
+				    "progress-bar-success",
+				    "progress-bar-warning",
+				    "progress-partition-green",
+				    "partition-azure",
+				    "partition-blue",
+				    "partition-orange",
+				    "partition-purple",
+				    "progress-bar-danger",
+				    "progress-bar-success",
+				    "progress-partition-green",
+				    "partition-purple"
+			    );
+			    
+			    $this->load->model("singleTeacherModel");
+			   $v1=$this->session->userdata('username');
+                $this->db->where("username",$v1);
+                $eid=$this->db->get("employee_info");
+                $id= $eid->row();
+                $v = $id->id;
+			    $var = $this->singleTeacherModel->getTeacherLeave($v);
+			    
+			    
+			    $count = 1;
+			foreach($var->result() as $lv): ?>
 				<?php if($count%2==0){$rowcss="danger";}else{$rowcss ="warning";}?>
                      <tr class="<?php echo $rowcss;?>">
-											<td><?php echo $count;?></td>
-											<td><?php echo $lv->start_date;?></td>
-											<td><?php echo $lv->end_date;?></td>
-											<td><?php echo $lv->total_leave;?></td>
-											<td><?php echo strtoupper($lv->reason);?></td>
+		  			<td><?php echo $count;?></td>
+		  			<td><?php echo $lv->start_date;?></td>
+		  			<td><?php echo $lv->end_date;?></td>
+		  			<td><?php echo $lv->total_leave;?></td>
+		  			<td><?php echo strtoupper($lv->reason);?></td>
 		  			<?php if($lv->status==0){?>
 		  				<input type="hidden" id="rowid" value="<?php echo $lv->id;?>">
-											<td><button type="submit" id="leavedelete"
-													class="btn btn-red">Cancle leave</button></td>
+		  			<td><button type="submit" id ="leavedelete" class="btn btn-red">
+				           Cancle leave
+				</button></td>
 			<?php }else{?>
-           <td><button type="submit" class="btn btn-yellow">Approved</button></td>
+           <td><button type="submit"  class="btn btn-yellow">
+				          Approved
+				</button></td>
             
 			<?php } ?>
 		  		</tr>
 		  		<?php $count++; endforeach; ?>
 		  		
 			</tbody>
-
-								</table>
-								<?php }else{
-									?>
-															<div class = "alert alert-warning"><strong><h1>Leave Record Not Found!!!!!</h1></strong></div>
-															
-							<?php 	}?>
-								<div>
-									<div class="col-sm-2 col-sm-offset-8">
-										<button type="submit" id="sonu"
-											class="btn btn-green next-step btn-block">
-											Define New Leave <i class="fa fa-arrow-circle-left"></i>
-										</button>
-									</div>
-
-									<div id="stuLeave">
-										<form
-											action="<?php echo base_url();?>index.php/singleTeacherControllers/insertLeave"
-											method="post">
-											<div class="form-group">
-												<div class="row">
-													<div class="col-md-6">
-														<label class="col-md-5 control-label"> Start Date <span
-															class="symbol required"></span>
-														</label>
-														<div class="col-md-7 form-group">
-															<input type="date" data-date-format="yyyy-mm-dd"
-																data-date-viewmode="years" name="sdate"
-																value="<?php echo set_value('empDob'); ?>"
-																class="form-control date-picker" required="required" />
-														</div>
-													</div>
-
-
-													<div class="col-md-6">
-														<label class="col-md-5 control-label"> End Date <span
-															class="symbol required"></span>
-														</label>
-														<div class="col-md-7 form-group">
-															<input type="date" data-date-format="yyyy-mm-dd"
-																data-date-viewmode="years" name="edate"
-																value="<?php echo set_value('empDob'); ?>"
-																class="form-control date-picker" required="required" />
-														</div>
-
-													</div>
-												</div>
-
-												<div class="row">
-													<div class="col-md-6">
-														<label class="col-md-5 control-label"> Total Leave </label>
-														<div class="col-sm-7 form-group">
-															<input type="number" class="form-control" id="totalLeave"
-																name="totalLeave" required="required"
-																value="<?php echo set_value('empPhoneNumber'); ?>">
-														</div>
-													</div>
-													<div class="col-md-6">
-														<label class="col-md-5 control-label"> Reason </label>
-														<div class="col-md-7 form-group ">
-															<input type="text" class="form-control text-uppercase"
-																required="required" id="reason" name="reason"
-																value="<?php echo set_value('empPhoneNumber'); ?>"
-																title="Only alphabate allow">
-														</div>
-													</div>
-												</div>
-
-												<div class="col-sm-2 col-sm-offset-8">
-													<button type="submit"
-														class="btn btn-blue next-step btn-block">
-														Submit <i class="fa fa-arrow-circle-left"></i>
-													</button>
-												</div>
-											</div>
-										</form>
-
-									</div>
-
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- end: panel Body -->
+			
+		</table>
+		<div><div class="col-sm-2 col-sm-offset-8">
+				<button type="submit" id = "sonu" class="btn btn-green next-step btn-block">
+				Define New Leave <i class="fa fa-arrow-circle-left"></i>
+				</button>
 			</div>
-			<!-- end: panel panel-white -->
-
-		</div>
-		<!-- end: MAIN PANEL COL-SM-12 -->
-	</div>
-	<!-- end: PAGE ROW-->
+			
+								<div id="stuLeave">
+								<form action="<?php echo base_url();?>index.php/singleTeacherControllers/insertLeave" method ="post">
+										<div class="form-group">
+<div class="row">
+<div class="col-md-6">
+<label class="col-md-5 control-label">
+Start Date  <span class="symbol required"></span>
+</label>
+<div class="col-md-7 form-group">
+<input type="date" data-date-format="yyyy-mm-dd" data-date-viewmode="years" name="sdate" value="<?php echo set_value('empDob'); ?>" class="form-control date-picker" required="required"/>
 </div>
+</div>
+
+
+<div class="col-md-6">
+<label class="col-md-5 control-label">
+End Date <span class="symbol required"></span>
+</label>
+<div class="col-md-7 form-group">
+<input type="date" data-date-format="yyyy-mm-dd" data-date-viewmode="years"  name="edate" value="<?php echo set_value('empDob'); ?>" class="form-control date-picker" required="required"/>
+</div>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-6">
+<label class="col-md-5 control-label">
+Total Leave  
+</label>
+<div class="col-sm-7 form-group">
+<input type="number" class="form-control" id="totalLeave" name="totalLeave"  required="required" value="<?php echo set_value('empPhoneNumber'); ?>" >
+</div>
+</div>
+<div class="col-md-6">
+<label class="col-md-5 control-label">
+Reason  
+</label>
+<div class="col-md-7 form-group ">
+<input type="text" class="form-control text-uppercase"  required="required" id="reason" name="reason" value="<?php echo set_value('empPhoneNumber'); ?>" title="Only alphabate allow" >
+</div>
+</div>
+</div>
+
+<div class="col-sm-2 col-sm-offset-8">
+<button type="submit" class="btn btn-blue next-step btn-block">
+Submit <i class="fa fa-arrow-circle-left"></i>
+</button>
+</div>
+</div>
+	</form>
+											
+											</div>
+		
+		</div>
+		</div>
+	</div>		
+	</div>
+			</div><!-- end: panel Body -->
+		</div><!-- end: panel panel-white -->
+		
+	</div><!-- end: MAIN PANEL COL-SM-12 -->
+</div><!-- end: PAGE ROW-->
+	</div>				
